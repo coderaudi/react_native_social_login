354a6e3f972c69ee54d26ff373cc93bc7b13f6fb
Abhijeet Khire <abhijeet.khire@Eternus.local>
2020-08-07 16:24:01 +0530

    basic

---
+++ screens/googlelogin.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View , Text } from 'react-native';
+
+
+const GoogleLogin = (params) => {
+  
+    return <View>
+        <Text>
+            google login
+        </Text>
+    </View>
+};
+
+
+export default GoogleLogin;
